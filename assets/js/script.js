b08882f6e0ac286a435da1e8b122eebf194e9e33
saad-sultan57 <muhammadsaad5758@gmail.com>
2024-09-08 00:34:27 +0500

    initial commit 4

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,50 @@
 $(document).ready(function () {
-  var owl = $(".owl-carousel").owlCarousel({
+  const owl = $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
-    nav: false, // Disable default navigation
-    responsive: {
-      0: {
-        items: 1,
-      },
-      500: {
-        items: 2,
-      },
-      1000: {
-        items: 2,
-      },
-    },
-    // autoplay: true,
+    nav: false,
+    responsive: { 0: { items: 1 }, 500: { items: 1 }, 1000: { items: 2 } },
+    autoplay: true,
     autoplayTimeout: 3000,
     autoplayHoverPause: true,
   });
 
-  // Custom button events
-  $(".owl-prev").click(function () {
-    owl.trigger("prev.owl.carousel");
-  });
+  $(".owl-prev").click(() => owl.trigger("prev.owl.carousel"));
+  $(".owl-next").click(() => owl.trigger("next.owl.carousel"));
 
-  $(".owl-next").click(function () {
-    owl.trigger("next.owl.carousel");
-  });
+  // Select first button and load 'healthy' images by default
+  const firstBtn = $(".nav-button").first();
+  loadImages("healthy", firstBtn[0]);
 });
+
+const images = {
+  healthy: ["assets/images/award1.png", "assets/images/healthy2.png"],
+  quality: ["assets/images/award2.png", "assets/images/quality2.png"],
+  infused: ["assets/images/award1.png", "assets/images/infused2.png"],
+};
+
+function loadImages(category, btn) {
+  // Update carousel images
+  $("#awards-carousel")
+    .empty()
+    .append(
+      images[category].map(
+        (src) => `<div class="item"><img src="${src}" class="w-75"></div>`
+      )
+    );
+
+  $(".owl-carousel")
+    .owlCarousel("destroy")
+    .owlCarousel({
+      loop: true,
+      margin: 10,
+      nav: false,
+      responsive: { 0: { items: 1 }, 500: { items: 1 }, 1000: { items: 2 } },
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+    });
+
+  $(".nav-button").removeClass("bg-olive-green text-white");
+  $(btn).addClass("bg-olive-green text-white");
+}
