1e13b730abbd3b19ad05f035bcc504acf57f5b2a
saad-sultan57 <muhammadsaad5758@gmail.com>
2024-09-08 16:10:00 +0500

    issues fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,8 @@ $(document).ready(function () {
   $(".owl-prev").click(() => owl.trigger("prev.owl.carousel"));
   $(".owl-next").click(() => owl.trigger("next.owl.carousel"));
 
-  // Select first button and load 'healthy' images by default
-  const firstBtn = $(".nav-button").first();
-  loadImages("healthy", firstBtn[0]);
+  // Load 'healthy' images by default
+  loadImages("healthy", $(".nav-button").first()[0]);
 });
 
 const images = {
@@ -45,6 +44,62 @@ function loadImages(category, btn) {
       autoplayHoverPause: true,
     });
 
+  // Remove active class from all buttons and add it to the clicked button
   $(".nav-button").removeClass("bg-olive-green text-white");
   $(btn).addClass("bg-olive-green text-white");
 }
+// Load the YouTube API
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+var isMuted = true;
+
+// Function to create YouTube player
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("myVideo", {
+    events: {
+      onReady: onPlayerReady,
+    },
+  });
+}
+
+// Function to play video
+function onPlayerReady(event) {
+  document
+    .getElementById("myModal")
+    .addEventListener("shown.bs.modal", function () {
+      event.target.playVideo();
+    });
+
+  // Add click event listener to mute toggle button
+  document.getElementById("muteToggle").addEventListener("click", function () {
+    if (isMuted) {
+      player.unMute();
+      player.setVolume(100);
+      this.innerHTML = '<i class="fas fa-microphone"></i>';
+    } else {
+      player.mute();
+      this.innerHTML = '<i class="fas fa-microphone-slash"></i>';
+    }
+    isMuted = !isMuted;
+  });
+}
+
+// Function to stop and mute the video
+function stopVideo() {
+  if (player && typeof player.stopVideo === "function") {
+    player.stopVideo();
+    player.mute();
+    isMuted = true;
+    document.getElementById("muteToggle").innerHTML =
+      '<i class="fas fa-microphone-slash"></i>';
+  }
+}
+
+// Add event listener to stop video when modal is hidden
+document
+  .getElementById("myModal")
+  .addEventListener("hidden.bs.modal", stopVideo);
